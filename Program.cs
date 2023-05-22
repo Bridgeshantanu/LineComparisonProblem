55d230ddc8a58998922f657061f2c346ed085a11
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-22 14:06:14 +0530

    Add: calculate length code is added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            CalculateLength calculator = new CalculateLength();
+            calculator.calculatelength();
         }
     }
 }
